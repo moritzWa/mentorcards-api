b3374e7f8cbccc6866d5f722b0b389f094269395
Moritz Wallawisch <wallawitsch@gmail.com>
2020-04-25 18:14:06 +0200

    navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React from "react"
+
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Container } from "semantic-ui-react"
+
+import "semantic-ui-css/semantic.min.css"
 import "./App.css"
+
+import MenuBarTop from "./components/MenuBarTop"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <Router className="App">
+      {/* or <div class='ui container' ... */}
+      <Container>
+        <MenuBarTop />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   )
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
+      diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+      dolor sit amet.
+      nrietasntihensatiesntasientasnietasnitehasntiesnatsneitasnietanrietanrthi
+    </div>
+  )
+}
+
+export default Home
