02d5ea93bde15786cca0a0831cd11c845e67073f
Moritz Wallawisch <wallawitsch@gmail.com>
2020-04-26 21:22:07 +0200

    basic quote card

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,57 @@
 import React from "react"
+import { useQuery } from "@apollo/react-hooks"
+import gql from "graphql-tag"
+import { Grid } from "semantic-ui-react"
 
-const Home = () => {
+import QuoteCard from "../components/QuoteCard"
+
+function Home() {
+  const {
+    loading,
+    data: { getQuotes: quotes },
+  } = useQuery(FETCH_QUOTES_QUERY)
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-      sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-      diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-      erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-      rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-      dolor sit amet.
-      nrietasntihensatiesntasientasnietasnitehasntiesnatsneitasnietanrietanrthi
-    </div>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Quotes</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading quotes..</h1>
+        ) : (
+          quotes &&
+          quotes.map((quote) => (
+            <Grid.Column key={quote.id} style={{ marginBottom: 20 }}>
+              <QuoteCard quote={quote} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
   )
 }
+
+const FETCH_QUOTES_QUERY = gql`
+  {
+    getQuotes {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`
 
 export default Home
