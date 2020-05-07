ee6ad77e2afe859d129e5f23f0a8a27b047f9936
Moritz Wallawisch <wallawitsch@gmail.com>
2020-05-07 19:12:57 +0200

    fix2 for deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config.env" })
 
 const pubsub = new PubSub()
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 const server = new ApolloServer({
   typeDefs,
